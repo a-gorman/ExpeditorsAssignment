f409c32a41bdd4ab04b41cfb8e488992d7b464ea
a-gorman <gormanalex1@gmail.com>
2024-07-26 01:23:30 -0700

    Write some failing unit tests

--- src/test/java/com/example/demo/SaleLeadFileParserTests.java
+++ src/test/java/com/example/demo/SaleLeadFileParserTests.java
@@ -28,4 +28,39 @@ public class SaleLeadFileParserTests {
         var result = new SalesLeadFileParser().parse(input);
         assertThat(result.get(0).firstName()).isEqualTo(expectedValue);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Smith,smith",
+            "Williams,williams",
+            "Johnson,johnson",
+            "Jones,jones",
+            "Brown,brown",
+    })
+    public void Parser_Should_CorrectlyParse_LastName(String nameInput, String expectedValue) {
+        var input = String.format("\"Dave\",\"%1$s\",\"123 main st.\",\"seattle\",\"wa\",\"43\"", nameInput).lines();
+
+        var result = new SalesLeadFileParser().parse(input);
+        assertThat(result.get(0).lastName()).isEqualTo(expectedValue);
+    }
+
+    @ParameterizedTest
+    @CsvSource( value = {
+            "123 main st.,123 main st",
+            "123 Main St.,123 main st",
+            "234 2nd Ave.,234 2nd ave",
+            "234 2nd Ave,234 2nd ave",
+            "234 2nd Ave.,234 2nd ave",
+            "234 2nd Ave.,234 2nd ave",
+            "345 3rd Blvd., Apt. 200,345 3rd blvd, apt 200",
+            "345 3rd Blvd. Apt. 200,345 3rd blvd apt 200",
+            "'123 main st ',123 main st ",
+            "123 Main St.,123 main st",
+    })
+    public void Parser_Should_CorrectlyParse_Address(String address, String expectedValue) {
+        var input = String.format("\"Dave\",\"Smith\",\"%1$s\",\"seattle\",\"wa\",\"43\"", address).lines();
+
+        var result = new SalesLeadFileParser().parse(input);
+        assertThat(result.get(0).streetAddress()).isEqualTo(expectedValue);
+    }
 }
